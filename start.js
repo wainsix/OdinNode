fe166109f5c370c893b90ab556194c5bbda969ee
Wainsix <wainsix@gmail.com>
2024-03-13 08:19:15 +0200

    style_express

--- start.js
+++ start.js
@@ -1,30 +1,16 @@
-// hello.js
+const express = require('express');
+const app = express();
+const path = require('path');
 
-// Import the 'http' module
-const http = require('http');
-const fs = require('fs');
+// Palvele staattisia tiedostoja kansioista "public"
+app.use(express.static(path.join(__dirname, 'public')));
 
-// Create a server that responds with "Hello, World!" for all requests
-const server = http.createServer((req, res) => {
-// Jos pyydetään juuripolkua, palautetaan index.html
-if (req.url === '/') {
-  fs.readFile('index.html', 'utf8', (err, data) => {
-    if (err) {
-      res.writeHead(500, {'Content-Type': 'text/plain'});
-      res.end('Internal Server Error\n');
-    } else {
-      res.writeHead(200, {'Content-Type': 'text/html'});
-      res.end(data);
-    }
-  });
-} else {
-  // Muussa tapauksessa vastataan "Hello, World!"-viestillä
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello, World!\n');
-}
+// Palauta index.html pyydettäessä
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-// Listen on port 3000 and IP address 127.0.0.1
-server.listen(3000, '127.0.0.1', () => {
+// Kuuntele pyyntöjä portissa 3000
+app.listen(3000, () => {
   console.log('Server running at http://127.0.0.1:3000/');
 });

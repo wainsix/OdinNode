b028a4843d33a6e9292eb5c3bc46df8e82c85d4c
Wainsix <wainsix@gmail.com>
2024-03-14 19:11:25 +0200

    dom exercise 1 done

--- public/app.js
+++ public/app.js
@@ -14,6 +14,24 @@ redp.classList.add("redp");
 redp.textContent = "Hey I’m red!";
 redp.style.color = "red";
 
+const divborder = document.createElement("div");
+divborder.classList.add("divborder");
+divborder.setAttribute("id", "divborder");
+divborder.style.border = "2px solid black";
+divborder.style.background = "pink";
+
+const hindiv = document.createElement("h1");
+content.classList.add("hindiv");
+hindiv.textContent = "I'm in a div";
+
+const pindiv = document.createElement("p");
+content.classList.add("pindiv");
+pindiv.textContent = "ME TOO!";
+
 container.appendChild(blueh);
 container.appendChild(redp);
 container.appendChild(content);
+container.appendChild(divborder);
+
+divborder.appendChild(hindiv);
+divborder.appendChild(pindiv);
